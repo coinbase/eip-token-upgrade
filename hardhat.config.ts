61057f5e4878afd086a8b2c7188c6ddbf4fe865d
john-peterson-coinbase <john.peterson@coinbase.com>
2022-03-15 20:05:07 -0400

    clean up hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,6 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import '@typechain/hardhat'
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   typechain: {
